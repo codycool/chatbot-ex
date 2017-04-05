7318e8262a2d82a6c62c652b53f65a8c055d0438
Cody Chen <codychen@Codyde-MacBook-Pro.local>
2017-04-05 20:24:23 +0800

    update index.js  by codycool

--- server/index.js
+++ server/index.js
@@ -9,3 +9,18 @@ var bot = linebot({
                       y4Xr+T2p5ZkwqbYkG6mdS1duTOgvncy8P/XIbxYFTh/V
                       kTfHob1nZJkl0YhF9QdB04t89/1O/w1cDnyilFU=
 });
+
+
+bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
+});
+
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
