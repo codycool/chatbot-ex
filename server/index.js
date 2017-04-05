022168f2bb5a0aeade994bf34ce0e6ef60e25a3c
Cody Chen <codychen@Codyde-MacBook-Pro.local>
2017-04-05 20:11:38 +0800

    initial project  by codycool

---
+++ server/index.js
@@ -0,0 +1,11 @@
+var linebot = require('linebot');
+var express = require('express');
+
+var bot = linebot({
+  channelId: 1506641103,
+  channelSecret: bf85b30866f47338100f664c7a296d75,
+  channelAccessToken: vZi+lMGhLUidaHAZsilvzEXzSFfRQaJXDiBScXGZA6RX
+                      tozRlz2c8Knt2q+lsAV/U6al2N0fEAGGucT5PnWi9wPO
+                      y4Xr+T2p5ZkwqbYkG6mdS1duTOgvncy8P/XIbxYFTh/V
+                      kTfHob1nZJkl0YhF9QdB04t89/1O/w1cDnyilFU=
+});
